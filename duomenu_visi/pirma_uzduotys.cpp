667ef1de825b13773187553eb3b88d2547b995f5
Arminas Pamparas <s066128@ad.viko.lt>
2026-09-17 11:33:52 +0300

    pirma uzduotys

---
+++ duomenu_visi/pirma_uzduotys.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+
+    string vardas ="1";
+    string pavarde="2";
+    int amzius= 3;
+    int grupe= 4;
+    int kursas= 5;
+    string studiju_programos_pavadinimas="6";
+
+
+
+
+    const auto lang = "C++";
+
+        cout << vardas <<" "<<  pavarde <<" "<< amzius <<" "<< grupe <<" "<< kursas <<" "<< studiju_programos_pavadinimas<< endl;
+
+
+    return 0;
+}
